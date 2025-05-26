ffdf3c5db9bd9c456d655529a150e8aa94d86c05
Deependra kumar <kushdep017@gmail.com>
2025-05-26 20:37:54 +0530

    Result page setup

--- InvestmentCalc/src/components/Result.jsx
+++ InvestmentCalc/src/components/Result.jsx
@@ -1,22 +1,42 @@
-export default function Result(){
-    return <table id="result">
-        <thead>
+import { calculateInvestmentResults, formatter } from "../util/investment";
+export default function Result() {
+  let tableData = calculateInvestmentResults({
+    initialInvestment: 15000,
+    annualInvestment: 900,
+    expectedReturn: 5.56,
+    duration: 10,
+  });
+  let totalInterest = 0;
+  let investedCapital=15000
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th scope="col">Year</th>
+          <th scope="col">Investment Value</th>
+          <th scope="col">Interest (Year)</th>
+          <th scope="col">Total Interest</th>
+          <th scope="col">Invested capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map((e, idx) => {
+          idx = idx > 0 ? idx - 1 : idx;
+          totalInterest = totalInterest + tableData[idx].interest;
+          investedCapital=investedCapital+e.annualInvestment
+          let data = (
             <tr>
-                <th scope="col">Year</th>
-                <th scope="col">Investment Value</th>
-                <th scope="col">Interest (Year)</th>
-                <th scope="col">Total Interest</th>
-                <th scope="col">Invested capital</th>
+              <td>{formatter.format(e.year)}</td>
+              <td>{formatter.format(e.valueEndOfYear)}</td>
+              <td>{formatter.format(e.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(investedCapital)}</td>
             </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>1600</td>
-                <td>1600900</td>
-                <td>900</td>
-                <td>1900</td>
-            </tr>
-        </tbody>
-    </table>;
+          );
+          return data;
+        })}
+      </tbody>
+    </table>
+  );
 }
