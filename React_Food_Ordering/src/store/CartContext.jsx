a815dcd5612787f8ce44b161945bc2e79f3f5f46
Deependra kumar <kushdep017@gmail.com>
2025-06-13 03:51:27 +0530

    Add Item using useReducer Done!

--- React_Food_Ordering/src/store/CartContext.jsx
+++ React_Food_Ordering/src/store/CartContext.jsx
@@ -1,17 +1,44 @@
-import { createContext } from "react";
+import { createContext, useReducer, useState } from "react";
 
 export const CartContext = createContext({
-    items:[],
-    addItem:(item)=>{},
-    removeItem:(id)=>{}
-})
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
 
-export function CartContextProvider({children}){
+function cartReducer(state, action) {
+  switch (action.type) {
+    case "ADD_ITEM": {
+      const existingItemIndex = state.items.findIndex(
+        (i) => i.id === action.item.id
+      );
+      const updatedItems = [...state.items];
 
-    return <CartContext>
+      if (existingItemIndex > -1) {
+        const existingItem = state.items[existingItemIndex];
+        const updatedItem = {
+          ...existingItem,
+          quantity: existingItem.quantity + 1,
+        };
+        updatedItems.push({...action.item,updatedItem})
+      } else {
+        updatedItems.push({ ...action.item, quantity: 1 });
+      }
+      return {...state,items:updatedItems}
+    }
+    case "REMOVE_ITEM": {
+    }
+    default: {
+      throw Error("Unknown action: " + action.type);
+    }
+  }
+  return state;
+}
 
-    </CartContext>
-} 
+export function CartContextProvider({ children }) {
+    const [ ] =  useReducer(cartReducer, { items: [] });
 
+  return <CartContext>{children}</CartContext>;
+}
 
-export default CartContext
+export default CartContext;
