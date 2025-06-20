ca4088e3924302645e0f3f43626a6477489e2ab7
Deependra kumar <kushdep017@gmail.com>
2025-06-21 02:42:46 +0530

    Notification modue

--- redux-in-action/client/src/App.js
+++ redux-in-action/client/src/App.js
@@ -3,10 +3,17 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import uiSlice from './store/ui-slice';
+import { useEffect } from 'react';
 
 
 function App() {
   const cartVisibilityState = useSelector(state => state.ui.isCartVisible)
+  const cart = useSelector((state) => state.cart)
+
+  useEffect(() => {
+    fetch('https://redux-cfa64-default-rtdb.firebaseio.com/cart.json', { method: 'PUT', body: JSON.stringify(cart) })
+  }, [cart])
+
 
   return (
     <Layout>
